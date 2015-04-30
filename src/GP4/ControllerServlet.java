55fdc9c50b5b7e233141de0fcdad79cbddcf1b12
HaojunZou <apollo_zhj@msn.com>
2015-04-30 12:22:54 +0200

    Servlets implementeras

--- src/GP4/ControllerServlet.java
+++ src/GP4/ControllerServlet.java
@@ -1,42 +1,20 @@
 package GP4;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @WebServlet("/ControllerServlet")
 public class ControllerServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    public ControllerServlet() {
-        super();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        User user = new User();
-        user.setName("arne");
-        user.setEmail("arne@parallell.com");
 
-        ArrayList<String> phoneNumbers = new ArrayList<String>();
-        phoneNumbers.add("555-123456");
-        phoneNumbers.add("555-789012");
-        phoneNumbers.add("555-345678");
-        user.setPhoneNumbers(phoneNumbers);
-
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/index.jsp");
-        request.setAttribute("user", user); // Request scope
-
-        dispatcher.forward(request, response);
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("name");
-        response.getWriter().write("Post: " + name);
-    }
-
 }
--- src/GP4/User.java
+++ src/GP4/User.java
@@ -1,36 +1,5 @@
 package GP4;
 
-import java.util.ArrayList;
-
 public class User {
-    private String name;
-    private String email;
-    private ArrayList<String> phoneNumbers;
 
-    public User(){
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String value){
-        name = value;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String value){
-        email = value;
-    }
-
-    public ArrayList<String> getPhoneNumbers(){
-        return phoneNumbers;
-    }
-
-    public void setPhoneNumbers(ArrayList<String> value){
-        phoneNumbers = value;
-    }
 }
